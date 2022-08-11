d97ce7ed81d4fb79dfbec4a7fb86656f00c2692c
megusto0 <ruslan00900@gmail.com>
2022-08-11 15:55:12 +0400

    hw

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    String name;
+    String middleName;
+
+    public Author(String name, String middleName) {
+        this.name = name;
+        this.middleName = middleName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    String title;
+    Author author;
+    int publishYear;
+
+    public Book(String title, Author author, int publishYear) {
+        this.title = title;
+        this.author = author;
+        this.publishYear = publishYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublishYear() {
+        return publishYear;
+    }
+
+    public void setPublishYear(int publishYear) {
+        this.publishYear = publishYear;
+    }
+}
